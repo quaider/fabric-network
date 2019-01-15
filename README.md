a68137bf463f160118238ffcf7156affc1eae65b
Kratos <pto.kratos@hotmail.com>
2019-01-15 16:11:45 +0800

    add demo chaincode and doc

--- README.md
+++ README.md
@@ -48,15 +48,20 @@ peer channel join -b cnabs.block
 ```
 
 ### Update Anchor peer node
+
 ```shell
-peer channel update -o orderer.cnabs.com:7050 -c cnabs -f /etc/hyperledger/configtx/Org1MSPanchors.tx --tls true --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/cnabs.com/orderers/orderer.cnabs.com/msp/tlscacerts/tlsca.cnabs.com-cert.pem
+peer channel update -o orderer.cnabs.com:7050 -c cnabs -f /etc/hyperledger/configtx/Org1MSPanchors.tx 
+
+#--tls true --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/cnabs.com/orderers/orderer.cnabs.com/msp/tlscacerts/tlsca.cnabs.com-cert.pem
 ```
-doc
+
 ## Chaincode
 
 ### Install chaincode
+
 这里也是使用的Admin用户的msp，特别需要注意的是GO语言的链码，不需要/opt/gopath/src全路径，只需要到github.com/chaincode就行了。
 非go语言的链码，则必须需要全路径，否则安装会报错（报错信息也没有指明是找不到链码，排错相当麻烦），如java需要的则需要全路径，到源码所在根目录
+
 ``` shell
 # GO环境直接是 github.com/chaincode/go，不需要全路径
 export CC_SRC_PATH=github.com/chaincode/
@@ -78,8 +83,62 @@ peer chaincode instantiate -o orderer.example.com:7050 -C $CHANNEL_NAME -n mycc 
 
 #### Query
 ``` shell
-peer chaincode query -C cnabs -n mycc -c '{"Args":["query","a"]}'
+peer chaincode query -C cnabs -n mycc -c '{"Args":["query","user1"]}'
 ```
+
+#### Invoke
+
+``` shell
+peer chaincode invoke -o orderer.cnabs.com:7050 -C cnabs -n mycc -c '{"Args":["invoke","user1","4"]}'
+```
+
+### Upgrade chaincode
+
+升级链码之前需要先对其进行安装，不然是找不到待升级的链码的
+
+``` shell
+# 安装新版本的链码
+peer chaincode install -n mycc -v 1.1 -l java -p ${CC_SRC_PATH}
+
+# 将链码升级到新版本
+# 改过程稍花时间，因为要启动新的链码容器
+peer chaincode upgrade -o orderer.cnabs.com:7050 -C cnabs -n mycc -v 1.1 -c '{"Args":["init"]}' -P "OR ('Org1MSP.peer','Org1MSP.admin','Org1MSP.member')"
+# --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA
+```
+
+## 调试模式
+
+正常情况下，每次更改链码都需要将链码安装到peer节点，升级链码，调试时只能依靠打日志的方式进行问题的排查，严重影响
+开发效率和开发体验。
+
+fabric提供了一个开发模式的开关，给链码部署和调试提供了便利，虽然还是稍显繁琐，但问题有所改善。
+
+开启链码调试功能的核心配置 是在 peer 节点的容器启动命令上 附加 --peer-chaincodedev=true，同时需暴露容器的
+7052端口给外部环境（chaincodes server的端口默认为7052，映射到宿主机可用端口即可，这里仍然以7052示例）
+
+``` yaml
+peer0.org1.cnabs.com:
+  container_name: peer0.org1.cnabs.com
+  image: hyperledger/fabric-peer
+  # ...
+  # 这个就是开启调试模式的关键所在了，注意必须开启一个端口 7052，用于外部链接
+  command: peer node start --peer-chaincodedev=true
+  ports: 
+    - 7051:7051
+    - 7052:7052
+    - 7053:7053
+```
+
+这样 fabric链码调试模式就准备就绪，由于我用的是java，所以需要在 idea 中设置2个关键环境变量，用于指定链码的标识和链码服务器，其中链码标识为 `name:version` 形式。
+
+``` property
+CORE_PEER_ADDRESS=192.168.8.131:7052
+CORE_CHAINCODE_ID_NAME=mycc:1.0
+```
+
+然后在 idea 中正常运行或调试，当在cli中执行链码调用时，链码会切换到本地编译器环境中，这样便可以断点调试了
+
+需要注意的是，在链码还未进行安装时，需要先手动安装、实例化链码（这一步是必须的，一般第一次才需要）
 
 ## 安装过程的错误
 * 链码安装报错如下
--- chaincode-demo/src/main/java/com/kratos/chaincode/demo/TestOneChaincode.java
+++ chaincode-demo/src/main/java/com/kratos/chaincode/demo/TestOneChaincode.java
@@ -1,19 +1,156 @@
 package com.kratos.chaincode.demo;
 
+import com.google.gson.*;
 import io.netty.handler.ssl.OpenSsl;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.hyperledger.fabric.shim.ChaincodeBase;
 import org.hyperledger.fabric.shim.ChaincodeStub;
+import org.hyperledger.fabric.shim.ledger.KeyModification;
+
+import java.util.Iterator;
 
 public class TestOneChaincode extends ChaincodeBase {
 
+    private static Log logger = LogFactory.getLog(TestOneChaincode.class);
+
+    private final String PLAYER_HP = "player_%s_hp";
+
     @Override
     public Response init(ChaincodeStub stub) {
-        return newSuccessResponse();
+        logger.info("chaincode 初始化方法执行...");
+        return newSuccessResponse("初始化成功...");
     }
 
     @Override
     public Response invoke(ChaincodeStub stub) {
+        String fun = stub.getFunction();
+        switch (fun) {
+            case "query":
+                return doQuery(stub);
+            case "invoke":
+                return doInvoke(stub);
+        }
+
+        return newSuccessResponse("invoke没有被处理...");
+    }
+
+    private Response doQuery(ChaincodeStub stub) {
+        Response response = Guard.checkArguments(stub, 2);
+        logger.info("query 参数检查结果：" + response.getStatus().name());
+        if (response.getStatus() != Response.Status.SUCCESS)
+            return response;
+
+        String user = stub.getParameters().get(0);
+
+        String compositeKey = String.format(PLAYER_HP, user);
+        // System.out.println("---compositeKey: " + compositeKey);
+
+        String hpStr = stub.getStringState(compositeKey);
+        System.out.println("current hp " + hpStr);
+
+        if (hpStr == null || hpStr.length() <= 0) {
+            hpStr = "100";
+        }
+
+        String hp = "hp is" + hpStr;
+        System.out.println(hp);
+
+        return newSuccessResponse(hp);
+    }
+
+    private Response doInvoke(ChaincodeStub stub) {
+
+        Response response = Guard.checkLessArguments(stub, 2);
+        if (response.getStatus() != Response.Status.SUCCESS)
+            return response;
+
+        String opType = stub.getParameters().get(0);
+        switch (opType) {
+            case "add":
+                return addHp(stub, true);
+            case "minus":
+                return addHp(stub, false);
+            case "history":
+                return fetchHistory(stub);
+        }
+
         return newSuccessResponse();
+    }
+
+    private Response addHp(ChaincodeStub stub, boolean doAdd) {
+        Response response = Guard.checkLessArguments(stub, 4);
+        if (response.getStatus() != Response.Status.SUCCESS)
+            return response;
+
+        int delta = 0;
+        try {
+            delta = Integer.parseInt(stub.getParameters().get(2));
+            if (delta > 100 || delta < 0) throw new NumberFormatException("hp must between 0 and 100");
+        } catch (NumberFormatException e) {
+            return newErrorResponse("your hp input is incorrect");
+        }
+
+        String user = stub.getParameters().get(1);
+        String compositeKey = String.format(PLAYER_HP, user);
+        int currentHp = 0;
+        try {
+            currentHp = Integer.parseInt(stub.getStringState(compositeKey));
+        } catch (NumberFormatException ex) {
+            currentHp = 100;
+        }
+
+        Integer newVal = doAdd ? (currentHp + delta) : (currentHp - delta);
+        if (newVal < 0) newVal = 0;
+        if (newVal > 100) newVal = 100;
+
+        stub.putStringState(compositeKey, newVal.toString());
+
+        return newSuccessResponse(newVal.toString());
+    }
+
+    private Response fetchHistory(ChaincodeStub stub) {
+        Response response = Guard.checkLessArguments(stub, 3);
+        if (response.getStatus() != Response.Status.SUCCESS)
+            return response;
+
+        String user = stub.getParameters().get(1);
+        String compositeKey = String.format(PLAYER_HP, user);
+
+        Iterator<KeyModification> modifications = stub.getHistoryForKey(compositeKey).iterator();
+
+
+        JsonArray jsonArray = new JsonArray();
+        while (modifications.hasNext()) {
+            KeyModification modification = modifications.next();
+            JsonObject o = new JsonObject();
+            o.addProperty("txid", modification.getTxId());
+            o.addProperty("value", modification.getStringValue());
+            o.addProperty("timestamp", modification.getTimestamp().getEpochSecond());
+            o.addProperty("deleted", modification.isDeleted());
+            jsonArray.add(o);
+        }
+
+        com.google.gson.Gson gson = new Gson();
+        String histryJson = gson.toJson(jsonArray);
+
+        return newSuccessResponse(histryJson);
+    }
+
+    static class Guard {
+        static Response checkArguments(ChaincodeStub stub, int n) {
+            int size = stub.getArgs().size();
+            if (size != n)
+                return newErrorResponse(String.format("参数个数不正确，期望是 %d", n));
+            return newSuccessResponse();
+        }
+
+        static Response checkLessArguments(ChaincodeStub stub, int n) {
+            int size = stub.getArgs().size();
+            if (size < n)
+                return newErrorResponse(String.format("参数个数不正确，期望是至少 %d", n));
+            return newSuccessResponse();
+        }
     }
 
     public static void main(String[] args) {
